e2ca80e90b52a32f9f75a8d12a79e448f4bef8e1
William Lewis <william.lewis@wework.com>
2018-02-25 16:30:54 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -31,3 +31,16 @@ def valid_move?(board, index)
 end
 
 
+def turn(board)
+  puts "Please enter 1-9:"
+  user_input = gets.strip
+  index = input_to_index(user_input)
+  if valid_move?(board, index)
+    move(board, index, current_player(board))
+    display_board(board)
+  else
+    turn(board)
+  end
+end
+
+
