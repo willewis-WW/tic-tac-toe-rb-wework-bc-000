4ffbe54f06bb02ed4b7dff04a8725ee8bc996ca3
William Lewis <william.lewis@wework.com>
2018-02-25 16:33:14 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -94,3 +94,13 @@ def winner(board)
 end
 
 
+def play(board)
+  while !over?(board)
+    turn(board)
+  end
+  if won?(board)
+    puts "Congratulations #{winner(board)}!"
+  elsif draw?(board)
+    puts "Cat's Game!"
+  end
+end
