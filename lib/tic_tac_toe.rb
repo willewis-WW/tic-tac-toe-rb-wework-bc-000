c1a5fd4fabfef80a35bcaef13cfb6104003c1960
William Lewis <william.lewis@wework.com>
2018-02-25 16:29:50 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -9,7 +9,11 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
+
 def input_to_index(user_input)
   user_input.to_i - 1
 end
 
+def move(board, index, player)
+  board[index] = player
+end
